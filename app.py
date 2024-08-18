b93413926d6d5c50323b8e57cc565011fa9a2023
Luiz Bueno <89259426+lcbueno@users.noreply.github.com>
2024-08-18 14:20:40 -0300

    Update app.py

--- app.py
+++ app.py
@@ -63,27 +63,31 @@ if st.sidebar.button("Vehicle Sales"):
 if st.sidebar.button("Customer Profile"):
     st.session_state['page'] = 'Perfil do Cliente'
 
-# Botão de upload do arquivo CSV abaixo dos botões de seleção de página
-uploaded_file = st.sidebar.file_uploader("Choose a CSV file", type="csv")
+# Botão de upload de múltiplos arquivos CSV
+uploaded_files = st.sidebar.file_uploader("Choose CSV files", type="csv", accept_multiple_files=True)
 
 # Inicializar o estado da sessão para a página principal
 if 'page' not in st.session_state:
     st.session_state['page'] = 'Overview Data'
 
-if uploaded_file is not None:
-    # Carregar o dataset com a codificação correta
-    df = pd.read_csv(uploaded_file, encoding='ISO-8859-1')
+if uploaded_files:
+    # Carregar e concatenar os datasets com a codificação correta
+    dfs = []
+    for uploaded_file in uploaded_files:
+        df = pd.read_csv(uploaded_file, encoding='ISO-8859-1')
+        dfs.append(df)
+    combined_df = pd.concat(dfs, ignore_index=True)
 
     # Converter a coluna "Date" para datetime sem exibir a mensagem de aviso
-    df['Date'] = pd.to_datetime(df['Date'], dayfirst=True, errors='coerce')
+    combined_df['Date'] = pd.to_datetime(combined_df['Date'], dayfirst=True, errors='coerce')
 
     # Remover qualquer linha com datas inválidas (NaT)
-    df = df.dropna(subset=['Date'])
+    combined_df = combined_df.dropna(subset=['Date'])
 
     # Aplicar filtros (sem mostrar no layout)
-    regions = df['Dealer_Region'].unique()
-    min_date = df['Date'].min().date()
-    max_date = df['Date'].max().date()
+    regions = combined_df['Dealer_Region'].unique()
+    min_date = combined_df['Date'].min().date()
+    max_date = combined_df['Date'].max().date()
     selected_region = regions  # Aplica automaticamente todas as regiões
     selected_dates = [min_date, max_date]  # Aplica automaticamente o intervalo completo
 
@@ -91,8 +95,8 @@ if uploaded_file is not None:
     selected_dates = pd.to_datetime(selected_dates)
 
     # Filtrando o DataFrame para todas as páginas
-    filtered_df = df[(df['Dealer_Region'].isin(selected_region)) & 
-                     (df['Date'].between(selected_dates[0], selected_dates[1]))]
+    filtered_df = combined_df[(combined_df['Dealer_Region'].isin(selected_region)) & 
+                              (combined_df['Date'].between(selected_dates[0], selected_dates[1]))]
 
     # Página: Visão Geral Dados
     if st.session_state['page'] == "Overview":
@@ -120,4 +124,4 @@ if uploaded_file is not None:
         st.title('Dashboard Yamaha - Customer Profile')
         # [seu código de visualização para Perfil do Cliente]
 else:
-    st.warning("Por favor, carregue um arquivo CSV para visualizar os dados.")
+    st.warning("Por favor, carregue um ou mais arquivos CSV para visualizar os dados.")
